9825622b82b418db9047c07032f5ed93f4258a2b
agustingomezdev <agustingomezdev@gmail.com>
2025-08-06 22:38:09 +0200

    add active style to current screen

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,10 +1,12 @@
-import { Link } from "@tanstack/react-router"
+import { Link, useLocation } from "@tanstack/react-router"
 import { Home, CreditCard, PieChart, Settings } from "lucide-react"
 
 const Navigation = () => {
+    const location = useLocation()
+
     const navItems = [
         { id: "/", icon: Home, label: "Inicio" },
-        { id: "transacciones", icon: CreditCard, label: "Transacciones" },
+        { id: "/transacciones", icon: CreditCard, label: "Transacciones" },
         { id: "/presupuestos", icon: PieChart, label: "Presupuestos" },
         { id: "/configuracion", icon: Settings, label: "Configuración" },
     ]
@@ -13,13 +15,15 @@ const Navigation = () => {
         <div className="dark:bg-gray-800 border-t dark:border-gray-700 fixed bottom-0 w-full">
             <div className="flex items-center justify-around max-w-7xl mx-auto container dark:text-white p-2">
                 {navItems.map((item) => {
-                    // const isActive = currentScreen === item.id
+                    const isActive = location.pathname === item.id
                     return (
                         <Link key={item.id} to={item.id}>
                             <button
                                 key={item.id}
-                                // onClick={() => onNavigate(item.id)}
-                                className={`flex flex-col items-center py-2 px-3 rounded-lg transition-all duration-200 hover:cursor-pointer`}
+                                className={`flex flex-col items-center py-2 px-3 rounded-lg transition-all duration-200 hover:cursor-pointer ${isActive
+                                        ? "text-slate-900 dark:text-white bg-slate-100 dark:bg-slate-700"
+                                        : "text-slate-500 dark:text-slate-400 hover:text-slate-700 dark:hover:text-slate-200"
+                                    }`}
                             >
                                 <item.icon className="w-5 h-5 mb-1" />
                                 <span className="text-xs font-medium">{item.label}</span>
